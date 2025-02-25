344809f228fdb26c4f00e3ffb98d6407cc1472a4
Hannah Anderson <339729345@GoTVDSB.ca>
2025-02-25 14:40:03 -0500

    Onto part 3!

--- Topic 4 -- Assignment/Program.cs
+++ Topic 4 -- Assignment/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Hannah Anderson
+
+            //Console.WriteLine("Part 1");
+            //String name;
+            //int age;
+            //Double salary;
+
+            //Console.WriteLine("Hello. What is your name?");
+            //name = Console.ReadLine();
+            //Console.WriteLine("Hi ," + name! + " How old are you?");
+            //Int32.TryParse(Console.ReadLine(), out age);
+            //Console.WriteLine("So you're " + age + " eh? That's not old at all! How much do you make, " + name);
+            //Double.TryParse(Console.ReadLine(), out salary);
+            //Console.WriteLine(salary.ToString("C") + "! I hope that's per hour and not per year!");
+
+            //Console.WriteLine("Part 2");
+            //String name, lastName, login;
+            //int grade, studentID; 
+            //Double average;
+
+            //Console.WriteLine("Name:");
+            //name = Console.ReadLine();
+            //Console.WriteLine("Last Name:");
+            //lastName = Console.ReadLine();
+            //Console.WriteLine("Login:");
+            //login = Console.ReadLine();
+            //Console.WriteLine("Grade:");
+            //Int32.TryParse(Console.ReadLine(), out grade);
+            //Console.WriteLine("Student ID:");
+            //Int32.TryParse(Console.ReadLine(), out studentID);
+            //Console.WriteLine("Average:");
+            //Double.TryParse(Console.ReadLine(), out average);
+
         }
     }
 }
