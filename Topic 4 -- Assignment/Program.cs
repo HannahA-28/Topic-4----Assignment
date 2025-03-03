d09ff4703b019d9dbfdd37aaabf336bea7ab09e2
Hannah Anderson <339729345@GoTVDSB.ca>
2025-03-03 13:32:58 -0500

    Finished

--- Topic 4 -- Assignment/Program.cs
+++ Topic 4 -- Assignment/Program.cs
@@ -9,47 +9,47 @@ namespace Topic_4____Assignment
         {
             //Hannah Anderson
 
-            //Console.WriteLine("Part 1");
-            //String name;
-            //int age;
-            //Double salary;
+            Console.WriteLine("Part 1");
+            String name;
+            int age;
+            Double salary;
 
-            //Console.WriteLine("Hello. What is your name?");
-            //name = Console.ReadLine();
-            //Console.WriteLine("Hi ," + name! + " How old are you?");
-            //Int32.TryParse(Console.ReadLine(), out age);
-            //Console.WriteLine("So you're " + age + " eh? That's not old at all! How much do you make, " + name);
-            //Double.TryParse(Console.ReadLine(), out salary);
-            //Console.WriteLine(salary.ToString("C") + "! I hope that's per hour and not per year!");
+            Console.WriteLine("Hello. What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hi ," + name! + " How old are you?");
+            Int32.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("So you're " + age + " eh? That's not old at all! How much do you make, " + name);
+            Double.TryParse(Console.ReadLine(), out salary);
+            Console.WriteLine(salary.ToString("C") + "! I hope that's per hour and not per year!");
 
-            //Console.WriteLine("Part 2");
-            //String name, lastName, login;
-            //int grade, studentID;
-            //Double average;
+            Console.WriteLine("Part 2");
+            String name, lastName, login;
+            int grade, studentID;
+            Double average;
 
-            //Console.WriteLine("Name:");
-            //name = Console.ReadLine();
-            //Console.WriteLine("Last Name:");
-            //lastName = Console.ReadLine();
-            //Console.WriteLine("Login:");
-            //login = Console.ReadLine();
-            //Console.WriteLine("Grade:");
-            //Int32.TryParse(Console.ReadLine(), out grade);
-            //Console.WriteLine("Student ID:");
-            //Int32.TryParse(Console.ReadLine(), out studentID);
-            //Console.WriteLine("Average:");
-            //Double.TryParse(Console.ReadLine(), out average);
+            Console.WriteLine("Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Last Name:");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Login:");
+            login = Console.ReadLine();
+            Console.WriteLine("Grade:");
+            Int32.TryParse(Console.ReadLine(), out grade);
+            Console.WriteLine("Student ID:");
+            Int32.TryParse(Console.ReadLine(), out studentID);
+            Console.WriteLine("Average:");
+            Double.TryParse(Console.ReadLine(), out average);
 
-            //Console.WriteLine("Part 3");
-            //String name;
-            //int age;
+            Console.WriteLine("Part 3");
+            String name;
+            int age;
 
-            //Console.WriteLine("Hello. What is your name?");
-            //name = Console.ReadLine();
-            //Console.WriteLine("Hi ," + name! + " How old are you?");
-            //Int32.TryParse(Console.ReadLine(), out age);
-            //Console.WriteLine("Did you know that in five years you will be " + (age + 5) +
-            //    "? And five years ago you were " + age + "! Imagine that!");
+            Console.WriteLine("Hello. What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hi ," + name! + " How old are you?");
+            Int32.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("Did you know that in five years you will be " + (age + 5) +
+                "? And five years ago you were " + age + "! Imagine that!");
 
             Console.WriteLine("Part 4");
             double one, two, three;
@@ -58,31 +58,33 @@ namespace Topic_4____Assignment
             Double.TryParse(Console.ReadLine(), out one);
             Double.TryParse(Console.ReadLine(), out two);
             Double.TryParse(Console.ReadLine(), out three);
+            double numbers = one + two + three;
+            Console.WriteLine(numbers / 2);
 
-            //Console.WriteLine("Part 5");
-            //String itemOne, itemTwo;
-            //Double priceOne, priceTwo, total, discount, discountTotal, tax, finalTotal;
+            Console.WriteLine("Part 5");
+            String itemOne, itemTwo;
+            Double priceOne, priceTwo, total, discount, discountTotal, tax, finalTotal;
 
-            //Console.WriteLine("Item 1:");
-            //itemOne = Console.ReadLine();
-            //Console.WriteLine("Price:");
-            //Double.TryParse(Console.ReadLine(), out priceOne);
-            //Console.WriteLine("Item 2:");
-            //itemTwo = Console.ReadLine();
-            //Console.WriteLine("Price:");
-            //Double.TryParse(Console.ReadLine(), out priceTwo);
-            //Console.WriteLine();
-            //total = priceOne + priceTwo;
-            //discount = total * 0.2;
-            //discountTotal = total - discount;
-            //tax = discountTotal * 0.13;
-            //finalTotal = discountTotal + tax;
-            //Console.WriteLine("Total: " + total.ToString("C"));
-            //Console.WriteLine("Discount: " + discount.ToString("C"));
-            //Console.WriteLine("Subtotal: " + discountTotal.ToString("C"));
-            //Console.WriteLine("Tax: " + tax.ToString("C"));
-            //Console.WriteLine();
-            //Console.WriteLine("Total Owed: " + finalTotal.ToString("C"));
+            Console.WriteLine("Item 1:");
+            itemOne = Console.ReadLine();
+            Console.WriteLine("Price:");
+            Double.TryParse(Console.ReadLine(), out priceOne);
+            Console.WriteLine("Item 2:");
+            itemTwo = Console.ReadLine();
+            Console.WriteLine("Price:");
+            Double.TryParse(Console.ReadLine(), out priceTwo);
+            Console.WriteLine();
+            total = priceOne + priceTwo;
+            discount = total * 0.2;
+            discountTotal = total - discount;
+            tax = discountTotal * 0.13;
+            finalTotal = discountTotal + tax;
+            Console.WriteLine("Total: " + total.ToString("C"));
+            Console.WriteLine("Discount: " + discount.ToString("C"));
+            Console.WriteLine("Subtotal: " + discountTotal.ToString("C"));
+            Console.WriteLine("Tax: " + tax.ToString("C"));
+            Console.WriteLine();
+            Console.WriteLine("Total Owed: " + finalTotal.ToString("C"));
 
         }
     }
